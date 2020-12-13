3c444c09a805d94cc3bdcfc1afca72f54741e943
leo-tavares <leonardotavaresdev@gmail.com>
2020-12-13 13:53:56 -0300

    exercicio: aula15 - Exibindo menu

---
+++ exercicios/aula15/ExibindoMenu/script.js
@@ -0,0 +1,7 @@
+function exibe(val, breakline) {
+  console.log(val + (breakline ? "\n" : ""));
+}
+exibe("Olá 👋! Bem vindo a Bebederia!");
+exibe("1 - Refrigerante 🥤");
+exibe("2 - Suco 🥤");
+exibe("3 - Água 🥤");
