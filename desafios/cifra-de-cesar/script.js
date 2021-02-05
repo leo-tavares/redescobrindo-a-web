58804907be6d8b214f5b6a2632cf4bb4d8acae0e
leo-tavares <leonardotavaresdev@gmail.com>
2021-02-04 23:05:57 -0300

    movendo projeto cifra de cesar da aula20
    (pode ser enontrado no repo exercicios_js na pasta aula20)

---
+++ desafios/cifra-de-cesar/script.js
@@ -0,0 +1,42 @@
+function getCharCodeListFromMessage(message) {
+  var charCodeList = [];
+  for (var index = 0; index < message.length; index++) {
+    var charCode = message[index].charCodeAt();
+    charCodeList.push(charCode);
+  }
+  return charCodeList;
+}
+
+function getShiftedMessage(shift, charCodeList) {
+  var shiftedMessage = [];
+  for (var index = 0; index < charCodeList.length; index++) {
+    var charCode = charCodeList[index];
+    shiftedMessage.push(charCode + shift);
+  }
+  return shiftedMessage;
+}
+
+function transformCharCodeListIntoString(charCodeList) {
+  var msg = "";
+  for (var index = 0; index < charCodeList.length; index++) {
+    var charCode = charCodeList[index];
+    msg += String.fromCharCode(charCode);
+  }
+  return msg;
+}
+
+function caesarCipher(shift, message) {
+  var charCodeList = getCharCodeListFromMessage(message);
+  var shiftedMessage = getShiftedMessage(shift, charCodeList);
+  var encryptedMessage = transformCharCodeListIntoString(shiftedMessage);
+  return encryptedMessage;
+}
+
+var userMsg = prompt("Informe a menssagem para encriptar");
+var userShift = parseInt(prompt("e qual o deslocamento?\n Informe apenas números"));
+
+var encryptedMessage = caesarCipher(userShift, userMsg);
+var decryptedMessage = caesarCipher(-userShift, encryptedMessage);
+
+console.log(`${userMsg} codificado com deslocamento ${userShift} => ${encryptedMessage}`);
+console.log(`${encryptedMessage} decodificada => ${decryptedMessage}`);
